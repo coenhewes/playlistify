77acf0f33a80ff7799e4e831377909ef73d2db7c
coenhewes <coenhewes@proton.me>
2024-07-19 14:12:25 +1000

    trying state management for username bug

--- src/components/AccountDetails/AccountDetails.jsx
+++ src/components/AccountDetails/AccountDetails.jsx
@@ -5,9 +5,10 @@ export default function AccountDetails({ avatar, setAvatar, userName, setUserNam
 	const CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
 	const REDIRECT_URI = import.meta.env.VITE_SPOTIFY_REDIRECT;
 	const DETAILS_ENDPOINT = "https://api.spotify.com/v1/me";
-
+	const [loading, setLoading] = useState(false)
 	// useEffect will probably be useful to get user details on load otherwise show logged out
 	useEffect(() => {
+		setLoading(true);
 		let bearerToken = window.localStorage.getItem("token"); 
 		const headers = { 'Authorization': `Bearer ${bearerToken}`};
 
@@ -17,9 +18,17 @@ export default function AccountDetails({ avatar, setAvatar, userName, setUserNam
 			setAvatar(data.images[0].url)
 			setUserName(data.display_name)
 			setUserId(data.id)
+			setLoading(false);
 		})
-
 	}, [])
+	// API Call
+	const displayUserName = () => {
+		if (loading) {
+			return <p>{userName}</p>
+		} else {
+			<p>Loading...</p>
+		}
+	}
 
 	// return basic account details using materialUI
 	return (
@@ -29,7 +38,8 @@ export default function AccountDetails({ avatar, setAvatar, userName, setUserNam
 					<Avatar alt="Remy Sharp" src={avatar} />
 				</div>
 				<div id="userName">
-					<p>{userName}</p>
+					<p>{displayUserName()}</p>
+					<p>{userId}</p>
 				</div>
 			</div>
 		</>
